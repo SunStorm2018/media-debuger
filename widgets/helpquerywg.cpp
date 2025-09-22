43744bd4bfcda0701b18df75d3f05e4bf3eebad7
zhanghongyuan <2063218120@qq.com>
2025-09-22 15:21:09 +0800

    feat: add search combox search in helpwg
    1. add search combox search in helpwg

--- widgets/helpquerywg.cpp
+++ widgets/helpquerywg.cpp
@@ -8,13 +8,15 @@ HelpQueryWg::HelpQueryWg(QWidget *parent)
     ui->setupUi(this);
     ui->category_combx->addItems(HELP_OPTION_FORMATS);
 
-    qDebug() << "get codecs-encoder:" << m_probe.getCodecsFromLibav(CODEC_TYPE_ENCODER);
-    qDebug() << "get codecs-decoder:" << m_probe.getCodecsFromLibav(CODEC_TYPE_DECODER);
-    qDebug() << "get muxer-demuxer:" << m_probe.getMuxersFromLibav(MUXER_TYPE_DEMUXER);
-    qDebug() << "get muxer-muxer:" << m_probe.getMuxersFromLibav(MUXER_TYPE_MUXER);
-    qDebug() << "get filters:" << m_probe.getFiltersFromLibav();
-    qDebug() << "get bsf:" << m_probe.getBsfFromLibav();
-    qDebug() << "get protocols:" << m_probe.getProtocolFromLibav();
+    // qDebug() << "get codecs-encoder:" << m_probe.getCodecsFromLibav(CODEC_TYPE_ENCODER);
+    // qDebug() << "get codecs-decoder:" << m_probe.getCodecsFromLibav(CODEC_TYPE_DECODER);
+    // qDebug() << "get muxer-demuxer:" << m_probe.getMuxersFromLibav(MUXER_TYPE_DEMUXER);
+    // qDebug() << "get muxer-muxer:" << m_probe.getMuxersFromLibav(MUXER_TYPE_MUXER);
+    // qDebug() << "get filters:" << m_probe.getFiltersFromLibav();
+    // qDebug() << "get bsf:" << m_probe.getBsfFromLibav();
+    // qDebug() << "get protocols:" << m_probe.getProtocolFromLibav();
+
+    emit ui->category_combx->activated(0);
 }
 
 HelpQueryWg::~HelpQueryWg()
@@ -49,13 +51,47 @@ void HelpQueryWg::on_search_btn_clicked()
             .arg(ui->param_combox->currentText())
     };
 
-    m_probe.getHelp(helpList);
+    ui->search_output_ple->setPlainText(m_probe.getHelp(helpList));
 }
 
 
 void HelpQueryWg::on_category_combx_activated(int index)
 {
+    if (QStringList{"full", "long"}.contains( ui->category_combx->currentText())) {
+        ui->param_combox->setVisible(false);
+    } else {
+        ui->param_combox->setVisible(true);
+    }
 
+    ui->param_combox->clear();
+    if (ui->category_combx->currentText() == DECODER_FMT) {
+        ui->param_combox->addItems(m_probe.getCodecsFromLibav(CODEC_TYPE_DECODER));
+        return;
+    }
+    if (ui->category_combx->currentText() == ENCODER_FMT) {
+        ui->param_combox->addItems(m_probe.getCodecsFromLibav(CODEC_TYPE_ENCODER));
+        return;
+    }
+    if (ui->category_combx->currentText() == DEMUXER_FMT) {
+        ui->param_combox->addItems(m_probe.getMuxersFromLibav(MUXER_TYPE_DEMUXER));
+        return;
+    }
+    if (ui->category_combx->currentText() == MUXER_FMT) {
+        ui->param_combox->addItems(m_probe.getMuxersFromLibav(MUXER_TYPE_MUXER));
+        return;
+    }
+    if (ui->category_combx->currentText() == FILTER_FMT) {
+        ui->param_combox->addItems(m_probe.getFiltersFromLibav());
+        return;
+    }
+    if (ui->category_combx->currentText() == BSF_FMT) {
+        ui->param_combox->addItems(m_probe.getBsfFromLibav());
+        return;
+    }
+    if (ui->category_combx->currentText() == PROTOCOL_FMT) {
+        ui->param_combox->addItems(m_probe.getProtocolFromLibav());
+        return;
+    }
 }
 
 
