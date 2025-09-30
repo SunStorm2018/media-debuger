83912842622e8b17ac978a06f8aca67a69de9ce8
zhanghongyuan <2063218120@qq.com>
2025-09-30 22:33:09 +0800

    fix: modify some build dependence
    - modify some build dependence
    - update clean bash

--- clean_build_cache.sh
+++ clean_build_cache.sh
@@ -1,4 +1,4 @@
-rm *.o ui_* moc_* qrc_* Makefile MediaDebuger media-debuger *.qrc .qmake.stash
+rm *.o ui_* moc_* qrc_* Makefile MediaDebuger media-debuger *.qrc .qmake.stash media-debuger.debug
 rm -rf app_logs/ .qm/ 
 rm -rf debian/.debhelper/ debian/media-debuger/ debian/files debian/debhelper-build-stamp debian/media-debuger.debhelper.log debian/media-debuger.substvars
 clear
